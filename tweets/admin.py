322782cceb8de738a82c3d1b475accc335db560d
love29ana <59245627+SEONGBEOM-KIM@users.noreply.github.com>
2024-11-04 23:17:40 +0000

    Improve the admin of Tweet and Like models.

--- tweets/admin.py
+++ tweets/admin.py
@@ -2,6 +2,23 @@ from django.contrib import admin
 from .models import Tweet, Like
 
 
+class WordFilter(admin.SimpleListFilter):
+    title = "Contain 'Elon Musk'"
+    parameter_name = "word"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("elon_musk", "Elon Musk"),
+        ]
+
+    def queryset(self, request, tweets):
+        word = self.value()
+        if word:
+            return tweets.filter(payload__contains=word)
+        else:
+            tweets
+
+
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
 
@@ -11,8 +28,22 @@ class TweetAdmin(admin.ModelAdmin):
         "count_likes",
     )
 
+    list_filter = (
+        WordFilter,
+        "created_at",
+    )
+
+    search_fields = (
+        "payload",
+        "user__username",
+    )
+
 
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
 
     list_display = ("__str__", )
+
+    list_filter = ("created_at", )
+
+    search_fields = ("user__username", )
